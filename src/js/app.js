0a23bbb20cf0a39524064326c0652a94ef749ab0
erezb <erezbiez@post.bgu.ac.il>
2018-11-14 20:57:18 +0200

    Code

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
 import $ from 'jquery';
 import {parseCode} from './code-analyzer';
+import {generateView} from './parsedCodeToView';
 
 $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
+        let view = generateView(parsedCode);
+        let table = document.getElementById('viewTable');
+        table.innerHTML = view;
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
     });
 });
